827e3a2f90889b47e0bf2892f1085e68885207bd
Robert.zhang <robertzhangsh@gmail.com>
2018-04-10 16:20:49 +0800

    Update MWPhotoBrowserPrivate.h

--- Pod/Classes/MWPhotoBrowserPrivate.h
+++ Pod/Classes/MWPhotoBrowserPrivate.h
@@ -8,7 +8,8 @@
 
 #import <UIKit/UIKit.h>
 #import <MBProgressHUD/MBProgressHUD.h>
-#import <MediaPlayer/MediaPlayer.h>
+#import <AVKit/AVKit.h>
+#import <AVFoundation/AVFoundation.h>
 #import "MWGridViewController.h"
 #import "MWZoomingScrollView.h"
 
@@ -54,7 +55,7 @@
     UIImage *_previousNavigationBarBackgroundImageLandscapePhone;
     
     // Video
-    MPMoviePlayerViewController *_currentVideoPlayerViewController;
+    AVPlayerViewController *_currentVideoPlayerViewController;
     NSUInteger _currentVideoIndex;
     UIActivityIndicatorView *_currentVideoLoadingIndicator;
     
